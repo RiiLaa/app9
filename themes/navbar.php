eb2917e3c679b6cba0bb8e2974d26a822d38089b
sandy prasetyo deniawan putra <sandypras707@gmail.com>
2022-06-20 13:22:26 +0700

    penambahan menu di navbar"matakuliah"

--- themes/navbar.php
+++ themes/navbar.php
@@ -21,6 +21,9 @@
       <li class="nav-item">
         <a class="nav-link" href="index.php?page=prodi">Prodi</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?page=matakuliah">Matakuliah</a>
+      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Dropdown
